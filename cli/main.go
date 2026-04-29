33026a247697ba05448b0a27526427268b604830
Rafael13579 <rafaelfernandesdasilva567@gmail.com>
2026-04-28 21:59:52 -0300

    feat: Adding imports

--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
 )
 
 func main() {
@@ -48,9 +54,47 @@ func main() {
 }
 
 func iniciarAssinador() {
-	fmt.Println("(Simulação: O Java subiria aqui)")
+	cmd := exec.Command("java", "-jar", "assinador.jar")
+
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("Falha ao iniciar o Java: %v\n", err)
+		return
+	}
+
+	pid := strconv.Itoa(cmd.Process.Pid)
+	err = ioutil.WriteFile("assinador.pid", []byte(pid), 0644)
+	if err != nil {
+		fmt.Printf("Java iniciou (PID %s), mas falhei ao salvar o arquivo de controle.\n", pid)
+	}
+
+	fmt.Printf("Assinador rodando em background! (PID: %s)\n", pid)
+	fmt.Println("Acesse em: http://localhost:8080")
 }
 
+
 func pararAssinador() {
-	fmt.Println("(Simulação: O Java pararia aqui)")
+	// 1. Lê o arquivo de PID
+	data, err := ioutil.ReadFile("assinador.pid")
+	if err != nil {
+		fmt.Println("❌ Erro: O Assinador não parece estar rodando (arquivo .pid não encontrado).")
+		return
+	}
+
+	pid, _ := strconv.Atoi(string(data))
+
+	// 2. Tenta encontrar o processo e matá-lo
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Printf("❌ Não encontrei o processo com PID %d.\n", pid)
+		return
+	}
+
+	err = process.Kill()
+	if err != nil {
+		fmt.Printf("❌ Falha ao encerrar o processo: %v\n", err)
+	} else {
+		fmt.Println("✅ Assinador encerrado com sucesso.")
+		os.Remove("assinador.pid") // Limpa o rastro
+	}
 }
